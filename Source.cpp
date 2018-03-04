789893db13ee3b78b869e1f6480b07913b11d970
dhvanil1996 <31262172+dhvanil1996@users.noreply.github.com>
2018-03-03 16:50:39 -0800

    ReUpload

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //Names: Dhvanil Shah
 //Due Date: 3/5/2018
 //Class: CPSC-323-04
@@ -14,7 +14,6 @@
 using namespace std;
 
 //Create variables to store keywords, operators, and separators
-const char keywords[] = { "function, int, boolean, real, if, endif, else, return, put, get, while, true, false" };
 const string oper = "+-*/";
 const string sps = "[]{}():;,";
 const string compound_ops = "=<>^";
@@ -464,77 +463,3 @@ int main() {
 	system("Pause");
 	return 0;
 }
-=======
-#include<iostream>	
-#include<string>
-#include<fstream>
-#include<cstring>
-#include<vector>
-
-
-using namespace std;
- 
-enum state {
-	a,
-	b,
-	c,
-	d,
-	e
-};
-
-int main() {
-
-	//int low, high, slice$;
-
-	vector<char> buffer;
-	vector<string> ids;
-	vector<char> ops;
-	state go = a;
-	fstream file;
-	
-	file.open("code.txt");
-
-	char i;
-	char *bb, *cc;
-	string aps = " ";
-	bb = cc = &buffer[0];
-	while (!file.eof()) {
-		file >> i;
-		buffer.push_back(i);
-	}
-	
-
-	int a = 0;
-	while (a<buffer.size()) {
-		if (isalpha(*cc)) {
-			go = b;
-		}if (isdigit(*cc)) {
-			go = c;
-			while (isdigit(*cc)) {
-				go = c; 
-				cc++;
-			}
-		}if (*cc == ' ' || *cc == ',' || *cc == ';') {
-			ops.push_back(*cc);
-			cc--;
-			while (bb != cc) {
-				aps += *bb;
-			}
-			cout << "ID = " << aps;
-		}
-		a++;
-	}
- 
-
-	 
-
-
-
-	cout << endl;
-	system("Pause");
-	return 0;
-}
-
-
- 
->>>>>>> 843ce588050b15da7bc6b7dee0f2f4e59d492313
